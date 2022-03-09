0137f9d04705822bcd855c2f51fa986afdd249c8
Jeremy Tellier <Jeremy@Actieve.com>
2022-03-09 10:57:57 -0500

    Add Scaled Identity View

---
+++ DesktopEdge/Views/Screens/IdentityView.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ziti.Desktop.Edge.Views.Screens {
+	public partial class IdentityView : Form {
+		public IdentityView() {
+			InitializeComponent();
+		}
+	}
+}
